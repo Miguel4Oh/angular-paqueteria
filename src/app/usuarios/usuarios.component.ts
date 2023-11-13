a4107f2d44de7cf05a00db84750c588786208896
Miguel Ortega <miguelortega2h@gmail.com>
2023-11-12 20:41:36 -0600

    primer commit

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-usuarios',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent {
+
+}
